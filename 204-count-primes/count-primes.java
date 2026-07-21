b40b9f333532df935921db54127f89f1528b3cfd
MEHAR BAJAJ <meharbajaj1607@gmail.com>
2026-07-21 17:21:13 +0530

    Time: 27 ms (99.57%) | Memory: 67 MB (12.78%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,26 +1,38 @@
 class Solution {
+       private static boolean[] isNotPrime = new boolean[5000001];
+    private static int[] primeCount = new int[5000001];
+    private static int maxProcessed = 1;
+
     public int countPrimes(int n) {
-        if (n <= 2) return 0;
+        if (n <= 2)
+            return 0;
 
-        boolean[] isPrime = new boolean[n];
-
-        for (int i = 2; i < n; i++) {
-            isPrime[i] = true;
+        if (n <= maxProcessed) {
+            return primeCount[n];
         }
 
-        for (int i = 2; i * i < n; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j < n; j += i) {
-                    isPrime[j] = false;
+        if (n > maxProcessed) {
+            if (maxProcessed == 1) {
+                isNotPrime[0] = isNotPrime[1] = true;
+                maxProcessed = 2;
+            }
+
+            for (int i = 2; i * i < n; i++) {
+                if (!isNotPrime[i]) {
+                    int start = Math.max(i * i, ((maxProcessed + i - 1) / i) * i);
+
+                    for (int j = start; j < n; j += i) {
+                        isNotPrime[j] = true;
+                    }
                 }
             }
+
+            for (int i = maxProcessed; i < n; i++) {
+                primeCount[i + 1] = primeCount[i] + (isNotPrime[i] ? 0 : 1);
+            }
+            maxProcessed = n;
         }
 
-        int count = 0;
-        for (int i = 2; i < n; i++) {
-            if (isPrime[i]) count++;
-        }
-
-        return count;
+        return primeCount[n];
     }
 }
