c99f21ecaa7f1676d63ca5fb61ca5f36b8589373
MEHAR BAJAJ <meharbajaj1607@gmail.com>
2026-07-21 22:09:56 +0530

    Time: 2192 ms (10.66%) | Memory: 51.5 MB (56.32%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count=0;
+        for(int i=0;i<g.length;i++){
+            for(int j=0;j<s.length;j++){
+                if(s[j]>=g[i]){
+                    count++;
+                    s[j] = Integer.MIN_VALUE;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
