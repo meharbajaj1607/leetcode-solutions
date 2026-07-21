39a94d109286fbdab05592f0477b17cf0b641fb5
MEHAR BAJAJ <meharbajaj1607@gmail.com>
2026-07-21 18:43:39 +0530

    Time: 1 ms (99.94%) | Memory: 47.3 MB (60.09%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -10,9 +10,6 @@ class Solution {
             }
         }
         for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
-        }
-        for(int i=0;i<arr.length;i++){
             if(arr[i]>1)freq[0] = (i+1);
             else if(arr[i]==0) freq[1] = (i+1);
         }
