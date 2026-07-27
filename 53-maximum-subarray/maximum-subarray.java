b32d52556eed82f4a3040c2642ba2d006eadc611
MEHAR BAJAJ <meharbajaj1607@gmail.com>
2026-07-27 11:47:57 +0530

    Time: 1 ms (99.95%) | Memory: 77.3 MB (32.28%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -5,6 +5,7 @@ class Solution {
         for (int i = 1; i < nums.length; i++) {
             currentSum = Math.max(nums[i], currentSum + nums[i]);
             maxSum = Math.max(maxSum, currentSum);
+            if(currentSum<0) currentSum=0;
         }
         return maxSum;
     }
