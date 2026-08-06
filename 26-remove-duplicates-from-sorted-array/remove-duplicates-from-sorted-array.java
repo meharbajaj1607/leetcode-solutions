a1ff452725ad5cd43ec62e4349c90b8ae5a16867
MEHAR BAJAJ <meharbajaj1607@gmail.com>
2026-08-06 23:45:00 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (9.52%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,3 +1,23 @@
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//         if (nums.length == 0) {
+//             return 0;
+//         }
+//         int count=0;
+//         HashSet<Integer>set = new HashSet<>();
+//         for(int i=0;i<nums.length;i++){
+//             if(!set.contains(nums[i])){
+//                 set.add(nums[i]);
+//                 count++;
+//             }
+//             else nums[i] = Integer.MAX_VALUE;
+//         }
+//         Arrays.sort(nums);
+//         return count;
+//     }
+// }
+
+//optimised
 class Solution {
     public int removeDuplicates(int[] nums) {
         
