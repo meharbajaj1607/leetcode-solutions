2a01b2190eae4221fc41f6ef467d22c0684aa079
MEHAR BAJAJ <meharbajaj1607@gmail.com>
2026-09-01 03:21:55 +0530

    Time: 3 ms (86.18%) | Memory: 43.3 MB (57.57%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,28 +1,42 @@
 class Solution {
     public boolean isValid(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
+//         Deque<Character> stack = new ArrayDeque<>();
 
-for (char c : s.toCharArray()) {
+// for (char c : s.toCharArray()) {
 
-    if (c == '(' || c == '[' || c == '{') {
-        stack.push(c);
-    }
-    else {
+//     if (c == '(' || c == '[' || c == '{') {
+//         stack.push(c);
+//     }
+//     else {
 
-        if (stack.isEmpty()) {
-            return false;
+//         if (stack.isEmpty()) {
+//             return false;
+//         }
+
+//         char top = stack.pop();
+
+//         if (c == ')' && top != '(' ||
+//             c == ']' && top != '[' ||
+//             c == '}' && top != '{') {
+//             return false;
+//         }
+//     }
+// }
+
+// return stack.isEmpty();
+        Stack<Character> st = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c=='['||c=='('||c=='{'){
+                st.push(c);
+            }
+            else{
+                if(st.isEmpty())return false;
+                char top = st.pop();
+                if((top=='['&&c!=']')||(top=='('&&c!=')')||(top=='{'&&c!='}')){
+                    return false;
+                }
+            }
         }
-
-        char top = stack.pop();
-
-        if (c == ')' && top != '(' ||
-            c == ']' && top != '[' ||
-            c == '}' && top != '{') {
-            return false;
-        }
-    }
-}
-
-return stack.isEmpty();
+        return st.isEmpty();
     }
 }
