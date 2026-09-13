0b33c96c00f37f6cb4c0e5de3f4689dc0faaf044
MEHAR BAJAJ <meharbajaj1607@gmail.com>
2026-09-13 06:03:16 +0530

    Time: 7 ms (8.94%) | Memory: 46.9 MB (33.72%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -9,34 +9,34 @@
  *     }
  * }
  */
-// public class Solution {
-//     public boolean hasCycle(ListNode head) {
-//         ListNode temp = head;
-//         HashSet<ListNode> set=new HashSet<>();
-//         while(temp !=null){
-//             if(set.contains(temp)){
-//                 return true;
-//             }
-//             set.add(temp);
-//             temp = temp.next;
-//         }
-//         return false;
-//     }
-// }
-
-//faster
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode fast=head;
-        ListNode slow=head;
-        while(fast!=null&&fast.next!=null){
-            fast=fast.next.next;
-            slow=slow.next;
-            if(fast==slow){
+        ListNode temp = head;
+        HashSet<ListNode> set=new HashSet<>();
+        while(temp !=null){
+            if(set.contains(temp)){
                 return true;
             }
-            
+            set.add(temp);
+            temp = temp.next;
         }
         return false;
     }
 }
+
+//faster
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         ListNode fast=head;
+//         ListNode slow=head;
+//         while(fast!=null&&fast.next!=null){
+//             fast=fast.next.next;
+//             slow=slow.next;
+//             if(fast==slow){
+//                 return true;
+//             }
+            
+//         }
+//         return false;
+//     }
+// }
