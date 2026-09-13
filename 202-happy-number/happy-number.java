522b9153a10999e63e614e51a242375ae13b5461
MEHAR BAJAJ <meharbajaj1607@gmail.com>
2026-09-13 06:20:52 +0530

    Time: 1 ms (77.40%) | Memory: 42.5 MB (34.07%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,21 +1,52 @@
+// class Solution {
+//     public int findSum(int n){
+//         int sum=0;
+//         while(n>0){
+//             int rem = n%10;
+//             n/=10;
+//             sum+=rem*rem;
+//         }
+//         return sum;
+//     }
+//     public boolean isHappy(int n) {
+//         if(n==1||n==7)return true;
+//         int sum=0;
+//         while(n>=10){
+//             sum = findSum(n);
+//             n = sum;
+//             if(n==1||n==7)return true;
+//         }
+//         return false;
+//     }
+// }
+
+//better
 class Solution {
-    public int findSum(int n){
-        int sum=0;
-        while(n>0){
-            int rem = n%10;
-            n/=10;
-            sum+=rem*rem;
+    public int findSum(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            int rem = n % 10;
+            n /= 10;
+            sum += rem * rem;
         }
+
         return sum;
     }
+
     public boolean isHappy(int n) {
-        if(n==1||n==7)return true;
-        int sum=0;
-        while(n>=10){
-            sum = findSum(n);
-            n = sum;
-            if(n==1||n==7)return true;
+        HashSet<Integer> seen = new HashSet<>();
+
+        while (n != 1) {
+            if (seen.contains(n)) {
+                return false;
+            }
+
+            seen.add(n);
+            n = findSum(n);
         }
-        return false;
+
+        return true;
     }
 }
+//An unhappy number eventually enters a repeating cycle. so return false when it happens
