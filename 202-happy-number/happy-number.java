eb6eef82c008bb1d9f47700b7544c65a06e4bd3b
MEHAR BAJAJ <meharbajaj1607@gmail.com>
2026-09-13 06:18:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (99.64%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findSum(int n){
+        int sum=0;
+        while(n>0){
+            int rem = n%10;
+            n/=10;
+            sum+=rem*rem;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        if(n==1||n==7)return true;
+        int sum=0;
+        while(n>=10){
+            sum = findSum(n);
+            n = sum;
+            if(n==1)return true;
+            if(n==1||n==7)return true;
+        }
+        return false;
+    }
+}
