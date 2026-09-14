1c336efa6048d197387a0778d1840d382920207b
MEHAR BAJAJ <meharbajaj1607@gmail.com>
2026-09-14 16:51:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (83.54%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,6 +1,25 @@
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         String[] arr = s.split(" ");
+//         return arr[arr.length-1].length();
+//     }
+// }
+
+
+//faster
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] arr = s.split(" ");
-        return arr[arr.length-1].length();
+        s=s.strip();
+        int count=0;
+        for(int i=s.length()-1;i>=0;i--){
+            char c= s.charAt(i);
+            if(c==' '){
+                break;
+            }
+            else{
+                count++;
+            }
+        }
+        return count;
     }
 }
