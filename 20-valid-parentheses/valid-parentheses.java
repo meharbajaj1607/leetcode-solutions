6da996d838187d523c17ea9db83cfa35ac071eb1
MEHAR BAJAJ <meharbajaj1607@gmail.com>
2026-09-20 07:37:02 +0530

    Time: 3 ms (85.92%) | Memory: 43.6 MB (15.14%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,29 +1,5 @@
 class Solution {
     public boolean isValid(String s) {
-//         Deque<Character> stack = new ArrayDeque<>();
-
-// for (char c : s.toCharArray()) {
-
-//     if (c == '(' || c == '[' || c == '{') {
-//         stack.push(c);
-//     }
-//     else {
-
-//         if (stack.isEmpty()) {
-//             return false;
-//         }
-
-//         char top = stack.pop();
-
-//         if (c == ')' && top != '(' ||
-//             c == ']' && top != '[' ||
-//             c == '}' && top != '{') {
-//             return false;
-//         }
-//     }
-// }
-
-// return stack.isEmpty();
         Stack<Character> st = new Stack<>();
         for(char c: s.toCharArray()){
             if(c=='['||c=='('||c=='{'){
