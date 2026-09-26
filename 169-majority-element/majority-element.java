ace079cc0171c52da4179ef943e30dc06e20c895
MEHAR BAJAJ <meharbajaj1607@gmail.com>
2026-09-26 22:11:16 +0530

    Time: 2 ms (72.78%) | Memory: 63.2 MB (7.03%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,23 +1,39 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            int key = nums[i];
-            if(map.containsKey(key)){
-                int value = map.get(key)+1;
-                map.put(key,value);
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     int key = nums[i];
+        //     if(map.containsKey(key)){
+        //         int value = map.get(key)+1;
+        //         map.put(key,value);
+        //     }
+        //     else map.put(key,1);
+        // }
+        // int num=-1;
+        // int max = Integer.MIN_VALUE;
+        // for(int i: map.keySet()){
+        //     int val = map.get(i);
+        //     if(max<val){
+        //         max = val;
+        //         num = i;
+        //     }
+        // }
+        // return num;
+        int n=nums.length;
+        int count=1;
+        int candidate=nums[0]; //majority ele
+        for(int i=1;i<n;i++){
+            if(count==0){
+                count=1;
+                candidate=nums[i]; //till now no majority ele
             }
-            else map.put(key,1);
+            else if(nums[i]==candidate)count++;
+            else count--;
         }
-        int num=-1;
-        int max = Integer.MIN_VALUE;
-        for(int i: map.keySet()){
-            int val = map.get(i);
-            if(max<val){
-                max = val;
-                num = i;
-            }
+        int actual=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==candidate)actual++;
         }
-        return num;
+        return (actual>n/2)?candidate:-1;
     }
 }
