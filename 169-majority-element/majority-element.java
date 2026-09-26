0ad8650553ab87d4c2a161c80596689939ea4212
MEHAR BAJAJ <meharbajaj1607@gmail.com>
2026-09-26 22:03:50 +0530

    Time: 2 ms (72.78%) | Memory: 63.3 MB (7.03%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,23 +1,35 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            int key = nums[i];
-            if(map.containsKey(key)){
-                int value = map.get(key)+1;
-                map.put(key,value);
-            }
-            else map.put(key,1);
-        }
-        int num=-1;
-        int max = Integer.MIN_VALUE;
-        for(int i: map.keySet()){
-            int val = map.get(i);
-            if(max<val){
-                max = val;
-                num = i;
-            }
-        }
-        return num;
+    // public int majorityElement(int[] nums) {
+    //     HashMap<Integer,Integer> map = new HashMap<>();
+    //     for(int i=0;i<nums.length;i++){
+    //         int key = nums[i];
+    //         if(map.containsKey(key)){
+    //             int value = map.get(key)+1;
+    //             map.put(key,value);
+    //         }
+    //         else map.put(key,1);
+    //     }
+    //     int num=-1;
+    //     int max = Integer.MIN_VALUE;
+    //     for(int i: map.keySet()){
+    //         int val = map.get(i);
+    //         if(max<val){
+    //             max = val;
+    //             num = i;
+    //         }
+    //     }
+    //     return num;
+    int majorityElement(int arr[]) {
+         int n=arr.length;
+    int candidate = arr[0], count = 1;
+    for (int i = 1; i < n; i++) {
+        if (count == 0) { candidate = arr[i]; count = 1; }
+        else if (arr[i] == candidate) count++;
+        else count--;
     }
+    int actual = 0;
+    for (int i = 0; i < n; i++) if (arr[i] == candidate) actual++;
+    return (actual > n / 2) ? candidate : -1;
+}
+    
 }
